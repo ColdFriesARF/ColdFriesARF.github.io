d3510254df88da3da02e0051696901103740fa37
PaperFenrir <92450243+PaperFenrir@users.noreply.github.com>
2021-12-07 19:09:16 -0600

    Add files via upload

--- test.php
+++ test.php
@@ -1,27 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<title>Automatic Remote Feeder</title>
+    <link rel="stylesheet" href="styles.css">
+	<script src="arf.js" defer></script>
+	
+	<style>
+		table, tr, td {
+			padding: 10px;
+			border: 2px solid black;
+			border-collapse: collapse;
+			text-align: center;
+		}
+		tr {
+			border: 4px solid black;
+		}
+		th {
+			font-weight: bold;
+		}
+		body {
+			text-align: center;
+		}
+		table {
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
+</head>
+
+<body> 
+	<?php
+		function convertDate($_date) {
+			$month = substr($_date, 5, 2);
+			switch ($month) {
+				case "1":
+					$month = "January";
+					break;
+				case "2":
+					$month = "Febuary";
+					break;
+				case "3":
+					$month = "March";
+					break;
+				case "4":
+					$month = "April";
+					break;
+				case "5":
+					$month = "May";
+					break;
+				case "6":
+					$month = "June";
+					break;
+				case "7":
+					$month = "July";
+					break;
+				case "8":
+					$month = "August";
+					break;
+				case "9":
+					$month = "September";
+					break;
+				case "10":
+					$month = "October";
+					break;
+				case "11":
+					$month = "November";
+					break;
+				case "12":
+					$month = "January";
+			}
+			$year = substr($_date, 0, 4);
+			$day = substr($_date, 8, 2);
+			$temp = "th";
+			switch ($day) {
+				case "1":
+				case "21":
+				case "31":
+					$temp = "st";
+					break;
+				case "2":
+				case "22":
+					$temp = "nd";
+					break;
+				case "3":
+				case "23":
+					$temp = "rd";
+					break;
+				default:
+					$temp = "th";
+			}
+			return $month . " " . $day . $temp . ", " . $year;
+		}
+	?>
+	
 	<?php 
+	
 		$servername = "localhost";
 		$username = "root";
-		$passowrd = "";
+		//$passowrd = "";
 		$database = "test";
 		
-		$conn = mysqli_connect($servername, $username, $password, $database);
+		//remember to change the "" back to $password once you figure out that issue
+		$conn = mysqli_connect($servername, $username, "", $database);
 		
 		if ($conn->connect_error) {
 			die("Database is not connect: error " . $conn->connect_error);
 		}
 		
-		$sql = "DELETE FROM `auto-feeder` WHERE id=9";
-		$result = $conn->query($sql);
+		//$sql = "DELETE FROM `auto-feeder` WHERE id=9";
+		//$result = $conn->query($sql);
 		
-		$sql = "SELECT * FROM `auto-feeder`";
+		//getting form data from index.php on what to insert into table
+		if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST) && !empty($_POST["amount"])) {
+			$date = $_POST["auto-date"];
+			$time = $_POST["auto-time"];
+			$amount = $_POST["amount"];
+		
+			//echo "<p>Dipsense $amount of food on $date at $time</p>";
+			
+			//inserts into the database the values obtained from the form in index.php
+			$sql = "INSERT INTO `auto-feeder` (`id`, `date`, `time`, `amount`) VALUES (NULL, '".$date."', '".$time."', ".$amount.")"; 
+			$result = $conn->query($sql);
+			
+			//redirect to same page to prevent form resubmittion
+			header('location: test.php');
+		}
+		$sql = "SELECT * FROM `auto-feeder` ORDER BY `id`";
 		$result = $conn->query($sql) or die($conn->error);
 		
+		echo "<p style='font-size:20pt'>Automatic Feed Times</p>";
+		
 		echo "<table>";
+		echo "<th><td>Date</td><td>Time</td><td>Amount</td></th>";
 		
 		while ($row = $result->fetch_assoc()) {
-			echo "<tr><td> Time " . $row['id'] . "</td><td>" . $row['date'] . "</td><td>" . $row['time'] . "</td><td>" . $row['amount'] . " Cup(s)</td></tr>";
+			echo "<tr><td> Item " . $row['id'] . "</td><td>" . /*$row['date']*/ convertDate($row['date']) . "</td><td>" . $row['time'] . "</td><td>" . $row['amount'] . " Cup(s)</td></tr>";
 		}
 		
-		echo "</table>";
+		echo "</table id='auto-times'>";
 
 	?>
+	
+	<a href="index.php" style="font-size:14pt"> Return </a>
+	
+</body>
+</html>
